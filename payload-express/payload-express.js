bcf1ed74155deb218f92ed72345fc264b098913c
Huib <huib.vandenbrink@mattr.global>
2020-12-08 08:20:59 +1300

    feat(payload-express) start ngrok and ask user for jws

--- payload-express/payload-express.js
+++ payload-express/payload-express.js
@@ -4,65 +4,89 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
+var jwsUrl;
 app.get('/qr', function (req, res) {
     const body = res.body
     console.log(jwsUrl)
     res.redirect(jwsUrl)
-  })
+})
 
 app.get('/test', function (req, res) {
     const body = res.body
     res.sendStatus(200)
-  })
-  
-  // listen on port 2001
-  app.listen(2001, function (err) {
+})
+
+// listen on port 2001
+app.listen(2001, function (err) {
     if (err) {
-      throw err
+        throw err
     }
-  
+
     console.log('Server started on port 2001')
-  })
+})
 
-var arg = process.argv.slice(2);
+// Start ngrok
+var ngrokUrl;
+const ngrok = require('ngrok');
+(async function () {
 
-
-// add jws from argument and the tenant from .env
-var jws = arg[0];
-var tenant = process.env.TENANT;
-var jwsUrl = `https://${tenant}.platform.mattr.global/?request=${jws}`;
-
-
+    ngrokUrl = await ngrok.connect(2001);
 
 // Construct the didcomm URL and check Ngrok from .env is running
-var https = require('https');
-var ngrokTest = `${process.env.NGROK}/test`
-https.get(ngrokTest, function(res) {
-    console.log("Ngrok statusCode: ", res.statusCode);
-})
+    var https = require('https');
+    var ngrokTest = `${ngrokUrl}/test`
+    let p = new Promise((resolve, reject) => {
+        const req = https.get(ngrokTest, function (res) {
+            console.log("Ngrok statusCode: ", res.statusCode);
+            resolve();
+        });
+        req.end();
+    });
+    await p;
 
-var didcommUrl = `didcomm://${process.env.NGROK}/qr`;
-console.log(didcommUrl);
+    // Ask the user for the JWS
+    const readline = require('readline');
+
+    function askQuestion(query) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        return new Promise(resolve => rl.question(query, answer => {
+            rl.close();
+            resolve(answer);
+        }))
+    }
+
+    console.log('ngrok callback URL to use in the presentation request: ', ngrokUrl + '/callback');
+    const jws = await askQuestion("Please provide the JWS");
+
+    // add jws from argument and the tenant from .env
+    var tenant = process.env.TENANT;
+    jwsUrl = `https://${tenant}.platform.mattr.global/?request=${jws}`;
+
+    var didcommUrl = `didcomm://${ngrokUrl}/qr`;
+    console.log(didcommUrl);
 
 // generate a QR Code using the didcomm url
-var QRCode = require('qrcode');
+    var QRCode = require('qrcode');
 
-QRCode.toString(didcommUrl,{type:'terminal'}, function (err, url) {
-    console.log(url)
-  })
-
-
+    QRCode.toString(didcommUrl, {type: 'terminal'}, function (err, url) {
+        console.log(url)
+    })
 
 // Receive the Callback
-const bodyParser = require('body-parser')
+    const bodyParser = require('body-parser')
 
 // Use body-parser middleware
-app.use(bodyParser.json())
+    app.use(bodyParser.json())
 
 // Receive a POST request to /callback & print it out to the terminal
-app.post('/callback', function (req, res) {
-  const body = req.body
-  console.log(body)
-  res.sendStatus(200)
-})
+    app.post('/callback', function (req, res) {
+        const body = req.body
+        console.log(body)
+        res.sendStatus(200)
+    })
 
+})();
