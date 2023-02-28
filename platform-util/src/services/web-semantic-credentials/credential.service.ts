6c72cf12486edc1ab7b0120c3b152121fb6ff0a2
Mingyang-Li-MATTR <mingyang.li@mattr.global>
2023-02-28 17:05:08 +1300

    feat(platform-util): added retrieveCredentials

--- platform-util/src/services/web-semantic-credentials/credential.service.ts
+++ platform-util/src/services/web-semantic-credentials/credential.service.ts
@@ -1,7 +1,7 @@
 import { IAuth } from '@/dto/setup';
 import { CreateCredentialArgs } from '@/dto/web-semantic-credentials/credentials';
 
-const createCredential = (auth: IAuth) => async (args: CreateCredentialArgs) => {
+const createCredential = (auth: IAuth) => async (args: CreateCredentialArgs): Promise<string> => {
   const resp = await fetch(`${auth.baseUrl}/core/v1/credentials`, {
     method: 'POST',
     headers: {
@@ -13,8 +13,25 @@ const createCredential = (auth: IAuth) => async (args: CreateCredentialArgs) => 
   return await resp.json();
 };
 
-const retrieveCredentials = (auth: IAuth) => async () => {
-  return { auth };
+const retrieveCredentials = (auth: IAuth) => async (args) => {
+  let url: string;
+    switch (args) {
+      case undefined:
+        url = `${auth.baseUrl}/core/v1/credentials`;
+      default:
+        const query = new URLSearchParams({
+          limit: args ? args?.query.pagination.limit.toString() : '1000',
+          cursor: args ? args?.query.pagination.cursor : '',
+        }).toString();
+        url = `${auth.baseUrl}/core/v1/credentials?${query}`;
+    }
+    const resp = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${auth.authToken}`,
+      },
+    });
+    return await resp.json();
 };
 
 const retrieveCredential = (auth: IAuth) => async () => {
