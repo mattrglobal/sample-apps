eb7eb5a6019b81c1a3e44d6957514dccacae9100
Mingyang Li <mingyang.li@mattr.global>
2023-06-30 16:54:57 +1200

    feat: connect function to router

--- e2e-issue-verify/src/server/api/routes/core.routes.ts
+++ e2e-issue-verify/src/server/api/routes/core.routes.ts
@@ -1,7 +1,17 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import {
+  issueStaticCredentialArgsSchema,
+  issueStaticCredentialResSchema,
+} from "@/types/issue-static-credential";
+import * as CoreService from "@/services/core.service";
 
 export const coreRoutes = createTRPCRouter({
-  issueStaticCredential: publicProcedure.query(() => ({ firedAt: Date.now() })),
+  issueStaticCredential: publicProcedure
+    .input(issueStaticCredentialArgsSchema)
+    .output(issueStaticCredentialResSchema)
+    .mutation(async ({ input }) => {
+      return await CoreService.issueStaticCredential(input);
+    }),
   createPresentationRequestQueryByExample: publicProcedure.query(() => ({
     firedAt: Date.now(),
   })),
---
+++ e2e-issue-verify/src/templates/IssueCredentialForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IssueCredentialForm = () => {
+  return <>IssueCredentialForm</>;
+};
+
+export default IssueCredentialForm;
