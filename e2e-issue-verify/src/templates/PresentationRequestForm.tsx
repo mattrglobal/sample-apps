7288af22f776b763b701e6ed75ab4a2adea5dbd7
Mingyang Li <mingyang.li@mattr.global>
2023-07-13 16:38:46 +1200

    feat: basic input field look & feel

--- e2e-issue-verify/src/templates/PresentationRequestForm.tsx
+++ e2e-issue-verify/src/templates/PresentationRequestForm.tsx
@@ -8,8 +8,8 @@ import QRCode from "react-qr-code";
 
 /**
  * Step 1: Create PresentationRequest
- * Step 2: Render QR code + start countdown timer + start fetching PresentationRequest.response
- * Step 3: Stop all of step 2 once PresentationRequest.response is non-empty
+ * Step 2: Render QR code + start countdown timer + start fetching PresentationRequest.response (long-polling)
+ * Step 3 (VERY IMPORTANT): Stop all of step 2 once PresentationRequest.response is non-empty
  * Step 4: Display PresentationRequest.response, show reset button
  * Step 5: Revert UI back to prior to step 1
  */
@@ -28,21 +28,21 @@ const PresentationRequestForm: FC = () => {
 
   return (
     <div>
-      <h1>PresentationRequest</h1>
+      <h1>Generate Presentation Request</h1>
       <form
         className="TODO_LATER"
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="auth-token">
+        <div id="auth-token" className="mb-5">
           <label htmlFor="auth-token">Auth token</label>
-          <input type="text" id="auth-token" {...register("token")} />
+          <input type="text" id="auth-token" {...register("token")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
-        <div className="tenant-domain">
+        <div id="tenant-domain" className="mb-5">
           <label htmlFor="tenant-domain">Tenant Domain</label>
-          <input type="text" id="tenant-domain" {...register("tenantDomain")} />
+          <input type="text" id="tenant-domain" {...register("tenantDomain")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
-        <button type="submit">
+        <button type="submit" className="bg-blue-500">
           {mutation.isLoading ? "Generating QR code..." : "Generate QR code"}
         </button>
       </form>
