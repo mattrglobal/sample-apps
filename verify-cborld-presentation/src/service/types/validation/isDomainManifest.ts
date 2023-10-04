e2e5fdfcb4b514797cd6e31f62b7392270c3379b
Preet Patel <preet@patel.studio>
2023-10-04 14:20:33 +1300

    chore(cbor-ld): delete verify-cborld-presentation app

--- verify-cborld-presentation/src/service/types/validation/isDomainManifest.ts
+++
@@ -1,7 +0,0 @@
-import { DomainManifest } from "../domainManifest";
-
-export const isDomainManifest = (val: any): val is DomainManifest =>
-  val.name &&
-  typeof val.name === "string" &&
-  (val.icons === undefined ||
-    (Array.isArray(val.icons) && val.icons.every((item: any) => typeof item.src === "string")));
