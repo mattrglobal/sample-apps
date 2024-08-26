583f1919b3794359a1a502e0d133e5a02f17cd92
Ralf Engbers <ralf.engbers@mattr.global>
2024-08-26 19:14:16 +1200

    feat: proper frontend and results rendering

--- mobile-credential-online-presentation/README.md
+++ mobile-credential-online-presentation/README.md
@@ -1,36 +1,16 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Mobile Credential Online Presentation Sample App
 
-## Getting Started
+This sample app demonstrates the usage of the Web Verifier SDK to present mobile credentials online on the same device and across devices.
+To run this app, you need
 
-First, run the development server:
+- A MATTR tenant with a presentation configuration.
+- Make this app publicly available, e.g. by using a ngrok or cloudflare tunnel.
+- A wallet with a mobile credential of doctype `org.iso.18013.5.1.mDL`.
 
+To run the app, call the following command in your terminal:
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
+and point your tunnel to `http://localhost:3000`.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+The app is a standard NextJS app, the main code on how to use the Web Verifier SDK can be found in `src/app/page.tsx`.
--- mobile-credential-online-presentation/src/app/page.tsx
+++ mobile-credential-online-presentation/src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
 import * as MATTRVerifierSDK from "@mattrglobal/verifier-sdk-web";
 import type { Claim } from "@mattrglobal/verifier-sdk-web/dist/typings/verifier/types";
+import { useCallback, useEffect, useState } from "react";
 
 type ResultError = { message: string };
 type ResultSuccess = { verified: boolean; claims: Record<string, string> };
 type Result = ResultSuccess | ResultError;
 
+// A helper function to extract the verification status and the claims form the PresentationSuccessResult
 function mapToResult(result: MATTRVerifierSDK.PresentationSuccessResult) {
-  const credential = result.credentials?.[0];
+  let credential: MATTRVerifierSDK.MobileCredentialPresentationCredential | undefined;
+  if ("credentials" in result) {
+    credential = result?.credentials?.[0];
+  }
 
   const isoClaims = (credential?.claims?.["org.iso.18013.5.1"] ?? {}) as Record<string, Claim>;
 
@@ -27,6 +31,7 @@ function mapToResult(result: MATTRVerifierSDK.PresentationSuccessResult) {
   } as Result;
 }
 
+// A helper function to map the binary image data to a data URI string for rendering
 function imageDataUri(data: string) {
   const signatures = {
     iVBORw0KGgo: "image/png",
@@ -41,11 +46,23 @@ function imageDataUri(data: string) {
 }
 
 export default function Home() {
-  MATTRVerifierSDK.initialise({ apiBaseUrl: "" });
   const [results, setResults] = useState<Result | null>(null);
 
-  const requestCredentials = useCallback(async () => {
+  // To request credentials, we need to
+  // 1. initialise the SDK,
+  // 2. repare the request options, and
+  // 3. call the requestCredentials function
+  const requestCredentials = useCallback(async (mode?: MATTRVerifierSDK.Mode) => {
     try {
+      const apiBaseUrl = localStorage.getItem("apiBaseUrl");
+      const redirectUri = localStorage.getItem("redirectUri");
+      if (!apiBaseUrl || !redirectUri) {
+        alert("Verifier tenant URL or redirect URI is missing");
+        return;
+      }
+
+      MATTRVerifierSDK.initialise({ apiBaseUrl: apiBaseUrl });
+
       const options: MATTRVerifierSDK.RequestCredentialsOptions = {
         credentialQuery: [
           {
@@ -62,10 +79,13 @@ export default function Home() {
           },
         ],
         challenge: MATTRVerifierSDK.utils.generateChallenge(),
-        redirectUri: `${process.env.NEXT_PUBLIC_APP_URL}/verification`,
+        redirectUri: redirectUri,
+        ...(mode && { mode: mode }),
         crossDeviceCallback: {
-          onSuccess: (callbackResponse: MATTRVerifierSDK.HandleRedirectCallbackResponse) => {
-            setResults(mapToResult(callbackResponse.result));
+          onSuccess: (callbackResponse) => {
+            if ("result" in callbackResponse) {
+              setResults(mapToResult(callbackResponse.result));
+            }
           },
           onFailure: (error) => {
             if (error.type === "Abort") {
@@ -75,6 +95,7 @@ export default function Home() {
           },
         },
       };
+
       await MATTRVerifierSDK.requestCredentials(options);
     } catch (e) {
       if (e instanceof Error) {
@@ -84,6 +105,16 @@ export default function Home() {
   }, []);
 
   const handleRedirect = useCallback(async () => {
+    // The API base URL must exist in localstorage so that we are able to handle the
+    // redirect and can intialise the SDK
+    const apiBaseUrl = localStorage.getItem("apiBaseUrl");
+    if (!apiBaseUrl) {
+      alert("Verifier tenant URL is missing");
+      return;
+    }
+    MATTRVerifierSDK.initialise({ apiBaseUrl: apiBaseUrl });
+
+    // A single function call handles the redirect callback for the same device flow
     const result = await MATTRVerifierSDK.handleRedirectCallback();
     if (result.isErr()) {
       setResults({ message: result.error.message });
@@ -92,22 +123,130 @@ export default function Home() {
 
     const presentationResult = result.value.result;
     if (!("sessionId" in presentationResult) || "error" in presentationResult) {
-      setResults({ message: "Something went wrong" })
+      setResults({ message: "Something went wrong" });
       return;
     }
 
     setResults(mapToResult(presentationResult));
   }, []);
 
+  // In the same device flow, we check if URL contains a hash and and handle the
+  // redirect when the page loads
   useEffect(() => {
     if (window.location.hash) {
+      console.log("Handling redirect");
       handleRedirect();
     }
   }, [handleRedirect]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello</div>
+    <main className="flex min-h-screen flex-col p-4 lg:p-24">
+      <h1 className="text-xl font-semibold tracking-tight py-8 lg:py-16 lg:text-3xl">
+        Mobile Credential Online Presentation
+      </h1>
+
+      <div className="flex flex-col gap-4 lg:flex-row lg:gap-16">
+        <div className="flex flex-col w-full lg:w-[50%]">
+          <h2 className="pb-2 lg:pb-4 text-lg lg:text-xl font-semibold tracking-tight">
+            Configuration
+          </h2>
+          <p className="pb-8 leading-wide">
+            Provide your verifier tenant URL and the redirect URL to request credentials. A MATTR
+            verifier tenant URL should look like{" "}
+            <code className="bg-slate-100 p-1 rounded">
+              https://your-tenant.vii.au01.mattr.global
+            </code>
+            . The redirect URL is the public URL of the sample app. If you are using ngrok or
+            cloudflare tunnels, it is the URL of the tunnel.
+          </p>
+
+          <div className="flex flex-col gap-y-4 w-full">
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="baseApi" className="font-semibold text-gray-600">
+                Verifier Tenant URL
+              </label>
+              <input
+                type="text"
+                id="baseApi"
+                defaultValue={localStorage.getItem("apiBaseUrl") || ""}
+                placeholder="https://your-tenant.vii.au01.mattr.global"
+                onChange={(e) => localStorage.setItem("apiBaseUrl", e.target.value)}
+                className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="redirectUri" className="font-semibold text-gray-600">
+                Redirect URL
+              </label>
+              <input
+                type="text"
+                id="redirectUri"
+                defaultValue={localStorage.getItem("redirectUri") || ""}
+                onChange={(e) => localStorage.setItem("redirectUri", e.target.value)}
+                placeholder="https://your-cloudflare-tunnel.trycloudflare.com"
+                className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+            </div>
+          </div>
+
+          <h2 className="pt-8 pb-2 lg:pb-4 text-lg lg:text-xl font-semibold tracking-tight">
+            Request credentials
+          </h2>
+          <div className="flex flex-row gap-x-2">
+            <button
+              type="submit"
+              className="px-4 py-2 rounded bg-black text-white hover:bg-gray-700d disabled:bg-gray-400"
+              onClick={() => requestCredentials()}
+            >
+              Auto mode detection
+            </button>
+            <button
+              type="button"
+              className="px-4 py-2 rounded bg-black text-white hover:bg-gray-700d disabled:bg-gray-400"
+              onClick={() => requestCredentials(MATTRVerifierSDK.Mode.sameDevice)}
+            >
+              Same device
+            </button>
+            <button
+              type="button"
+              className="px-4 py-2 rounded bg-black text-white hover:bg-gray-700d disabled:bg-gray-400"
+              onClick={() => requestCredentials(MATTRVerifierSDK.Mode.crossDevice)}
+            >
+              Cross device
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full lg:w-[50%]">
+          <h2 className="pt-16 lg:pt-0 pb-4 text-lg lg:text-xl font-semibold tracking-tight">
+            Results
+          </h2>
+          {!results && <p className="text-gray-500">No results yet</p>}
+          {results && "claims" in results && (
+            <div className="flex flex-col gap-4">
+              <img width={200} src={results.claims.image} alt="portrait" />
+              <div>
+                <div className="text-sm text-slate-500">Given Name</div>
+                <div>{results.claims.given_name}</div>
+              </div>
+              <div>
+                <div className="text-sm text-slate-500">Family Name</div>
+                <div>{results.claims.family_name}</div>
+              </div>
+              <div>
+                <div className="text-sm text-slate-500">Age over 18</div>
+                <div>{results.claims.age_over_18 ? "true" : "false"}</div>
+              </div>
+              <div>
+                <div className="text-sm text-slate-500">Presentation Status</div>
+                <div>{results.verified ? "verified" : "not verified"}</div>
+              </div>
+            </div>
+          )}
+          {results && "message" in results && <p className="text-red-500">{results.message}</p>}
+        </div>
+      </div>
     </main>
   );
 }
