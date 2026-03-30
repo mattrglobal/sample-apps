8d9c2561e498eb50a49a7f25b3238ecee693571a
Ralf Engbers <raleng@users.noreply.github.com>
2026-03-30 16:59:56 +1300

    chore: fix nextjs cross origin warning (#275)

--- verifier-web-tutorial/next.config.ts
+++ verifier-web-tutorial/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["*.ngrok-free.app"],
 };
 
 export default nextConfig;
