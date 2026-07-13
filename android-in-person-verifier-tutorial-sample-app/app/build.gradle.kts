18f2af07ae06602e8bff2adf3bfc6568ecbbee97
Pavel Vorotylenko <pavel.vorotylenko@mattr.global>
2026-07-14 11:35:29 +1200

    chore: fix minor android sample app issues

--- android-in-person-verifier-tutorial-sample-app/app/build.gradle.kts
+++ android-in-person-verifier-tutorial-sample-app/app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("global.mattr.mobilecredential:verifier:7.0.0")
+    implementation("global.mattr.mobilecredential:verifier:7.0.1")
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("com.google.accompanist:accompanist-permissions:0.36.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
